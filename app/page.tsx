fc79cc9793e4a62862a7fc49e1a493cb18d58acb
Sinisa Koscec <sinisa@dvasadva.com>
2023-10-20 12:32:30 +0200

    add getServerSession

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,3 @@
-"use client";
-import { useSession } from "next-auth/react";
-
 export default function Home() {
-  const { data: session, status } = useSession();
-  if (status === "authenticated") {
-    return <p>Signed in as {session?.user?.email}</p>;
-  }
-
-  return <a href="/api/auth/signin">Sign in</a>;
+  return <div>Home</div>;
 }
