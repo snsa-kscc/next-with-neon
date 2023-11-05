66361215791ff632583b61264cc03afea76f55c3
Sinisa Koscec <sinisa@dvasadva.com>
2023-11-05 01:24:30 +0100

    intl date

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,7 +4,9 @@ import { authOptions } from "../api/auth/[...nextauth]/route";
 import QuestionForm from "../components/QuestionForm";
 
 import { db } from "@/db";
-import { receipes } from "@/db/schema";
+import { receipes, users } from "@/db/schema";
+import { eq } from "drizzle-orm";
+import { formatTimeAgo } from "@/utils/formatTimeAgo";
 
 export default async function Dashboard() {
   const session: { user: { name: string } } | null = await getServerSession(authOptions);
@@ -13,7 +15,7 @@ export default async function Dashboard() {
     redirect("/api/auth/signin");
   }
 
-  const result = await db.select().from(receipes);
+  const result = await db.select().from(receipes).innerJoin(users, eq(receipes.userId, users.id));
 
   const res = await db.query.users.findFirst({
     with: {
@@ -27,14 +29,16 @@ export default async function Dashboard() {
       {!session.user.name ? <p>What is your name?</p> : null}
       <QuestionForm user={session.user.name} />
       <div className="p-8">
-        {result.map((receipe) => (
-          <div key={receipe.createdAt.toString()}>
-            <p>{receipe.description}</p>
+        {result.map((item) => (
+          <div className="flex flex-col" key={item.recipe.createdAt.toString()}>
+            <p>
+              {item.recipe.description} by {item.user.name} -
+            </p>
+            <p title={`${item.recipe.createdAt}`}>{formatTimeAgo(item.recipe.createdAt)}</p>
+            <hr />
           </div>
         ))}
       </div>
     </div>
   );
 }
-
-// 2DO - find many
